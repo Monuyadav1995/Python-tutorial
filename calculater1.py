ab3985c01a31cee5250641c33d420901f9851e0d
SRIKANT <srikant.a@bigdoorsolutions.co.in>
2024-08-10 14:52:29 +0530

    This is my first py project

---
+++ calculater1.py
@@ -0,0 +1,43 @@
+
+
+# I start a calculeter project.
+
+# addition...
+def sum(a, b):    # sum This is the name of the function.
+    c = a + b    # (a, b): These are the parameters of the function and c This is a variable that will store the result
+    return c     #  
+add=sum(20,80)
+print(f"sub of two no is: {add}")
+
+
+# substraction....
+def sub(a, b):
+    c = a - b
+    return c
+subtr=sub(80,30)
+print(f"sub of two no is: {subtr}")
+
+# multiplication....
+def mul(a , b):
+    c= a * b
+    return c
+cul=mul(30 ,80)
+print(f"multiplication of is :{cul}")
+
+
+
+# division....
+def div(a, b):
+    c = a / b
+    return c
+divi= div(50,2)
+print(f"div of two no is:{divi}")
+
+
+#modulus....
+
+# def mod (f , g):
+#    k=f % g
+#    return k
+# modulus = mod(1,2)
+# print(f"Modules of {modulus}")
